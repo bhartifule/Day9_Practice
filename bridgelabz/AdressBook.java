0a84d920be954f0b36c89dba182df05db163ec8f
Bharti <bharatifule10gmail.com>
2022-12-05 00:58:11 +0530

    create contact added

--- bridgelabz/AdressBook.java
+++ bridgelabz/AdressBook.java
@@ -1,8 +1,27 @@
 package com.bridgelabz;
+import java.util.PrimitiveIterator;
 
 public class AdressBook {
 	public static void main(String[] args) {
-		System.out.println("Well come Adress Book");
-	}
+        System.out.println("Welcome to Address Book Program");
+        AddressBookMain obj1 = new AddressBookMain();
+        obj1.setName("Bharti");
+        obj1.setSurname("Fule");
+        obj1.setAddress("Hingna Nagpur");
+        obj1.setCity("Nagpur");
+        obj1.setState("Maharastra");
+        obj1.setContactNumber("9146075923");
+        obj1.setEmail("shitalkarad@24gmail.com");
+
+        System.out.println("Name: " + obj1.getName());
+        System.out.println("Surname: " + obj1.getSurname());
+        System.out.println("Address: " + obj1.getAddress());
+        System.out.println("City: " + obj1.getCity());
+        System.out.println("State: " + obj1.getState());
+        System.out.println("ContactNumber: " + obj1.getContactNumber());
+        System.out.println("Email: " + obj1.getEmail());
+
+
+    }
 
 }
